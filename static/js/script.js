2c368429c37ca8377c37fcafcacf01ce5e543330
AayushDongre <aayushdongre4@gmail.com>
2019-04-04 23:09:49 +0530

    v2.0.3

--- static/js/script.js
+++ static/js/script.js
@@ -3,4 +3,16 @@ $( document ).ready(function() {
       $("#b").animate({top:"-50vh",height:"100vh"},500);
       $(".main").animate({top:"-30vh"},500);
     });
+    $(".upload").click(function(){
+      $(".indeterminate").addClass("blue-grey lighten-5");
+    });
+    $(".links").click(function(){
+      if($(".description").hasClass("attop")==true)
+      {
+        $(".description").removeClass("attop").animate({top:"100vh"},500);
+      }
+      else
+      $(".description").animate({top:"50vh"},500).addClass("attop");
+      
+    });
 });
