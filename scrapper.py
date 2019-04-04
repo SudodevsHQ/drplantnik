8d2b44d4f001bd768923dcb4ce542e2f3960b3e2
meta-boy <anurag4884@gmail.com>
2019-04-04 17:59:37 +0800

    v2.0.1

--- scrapper.py
+++ scrapper.py
@@ -16,7 +16,7 @@ class Google:
             query_string = {"key":key,"cx":cx,"num":"10","q":q}
         response = requests.request("GET", url, params=query_string)
         json_data = json.loads(response.text)
-        json_data = [json_data["items"][i]["link"] for i in range(10)]
+        json_data = [json_data["items"][i]["link"] for i in range(5)]
         return json_data
 
     # Google Command, clumsy but works~
