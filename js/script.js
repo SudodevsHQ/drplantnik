d1c883f66bb75a25dc286aca2c8e5169b5d488fc
therexone <renerjake@gmail.com>
2019-04-04 09:54:44 +0530

    Initial Commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+$( document ).ready(function() {
+    $(".getstarted").click(function(){
+      $("#b").animate({top:"-50vh",height:"100vh"},500);
+      $(".main").animate({top:"-30vh"},500);
+    });
+});
