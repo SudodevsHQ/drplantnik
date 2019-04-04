4d0c3fbd9b5ba8f96e1b4b4185e39ae027de37d4
AayushDongre <aayushdongre4@gmail.com>
2019-04-04 18:11:27 +0530

    Preloaders and shizz

--- js/script.js
+++ js/script.js
@@ -3,4 +3,7 @@ $( document ).ready(function() {
       $("#b").animate({top:"-50vh",height:"100vh"},500);
       $(".main").animate({top:"-30vh"},500);
     });
+    $(".upload").click(function(){
+      $(".indeterminate").addClass("blue-grey lighten-5");
+    });
 });
