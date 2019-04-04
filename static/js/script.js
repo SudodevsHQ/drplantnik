d294388fd8ad0381a367ffe6ddd432cd734a7ffa
AayushDongre <aayushdongre4@gmail.com>
2019-04-04 23:23:46 +0530

    quickfix

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,4 @@
 $( document ).ready(function() {
-<<<<<<< HEAD
     $(".getstarted").click(function(){
       $("#b").animate({top:"-50vh",height:"100vh"},500);
       $(".main").animate({top:"-30vh"},500);
@@ -16,14 +15,4 @@ $( document ).ready(function() {
       $(".description").animate({top:"50vh"},500).addClass("attop");
       
     });
-=======
-  $(".getstarted").click(function(){
-    $("#b").animate({top:"-50vh",height:"100vh"},500);
-    $(".main").animate({top:"-30vh"},500);
-  });
-  $(".upload").click(function(){
-    $(".indeterminate").addClass("blue-grey lighten-5");
-  });
-
->>>>>>> 8333dbf250ce3d41599704f8340435af7dcbb951
 });
